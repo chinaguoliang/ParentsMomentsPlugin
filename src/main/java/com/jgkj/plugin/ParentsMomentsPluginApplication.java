1c64957daedcc3d364b5ca25a755c671b7d77472
chen <38033919@qq.com>
2016-10-19 00:02:52 +0800

    Initial commit

---
+++ src/main/java/com/jgkj/plugin/ParentsMomentsPluginApplication.java
@@ -0,0 +1,12 @@
+package com.jgkj.plugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentsMomentsPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentsMomentsPluginApplication.class, args);
+	}
+}
