75f1c3e9bfa910e7edc145f98994a4e03747713a
chen <38033919@qq.com>
2016-10-20 04:29:33 +0800

    增加数据库交互

--- src/main/java/com/jgkj/plugin/controllers/LocationController.java
+++ src/main/java/com/jgkj/plugin/controllers/LocationController.java
@@ -1,13 +1,13 @@
 package com.jgkj.plugin.controllers;
 
 import com.jgkj.plugin.domain.Location;
-import com.jgkj.plugin.services.LocationMapper;
+import com.jgkj.plugin.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chen on 16/10/19.
@@ -15,12 +15,43 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LocationController {
     @Autowired
-    LocationMapper locationMapper;
+    LocationRepository locationRepository;
 
-    @RequestMapping(value = "/school/position",method= RequestMethod.POST)
+    @RequestMapping("/attendance/getLocation/schoolid={schoolid}")
     @ResponseBody
-    public Location getSchoolLocation(@RequestParam("schoolId") String schoolId) {
-        Location location = locationMapper.findUserBySchoolId(schoolId);
+    public Location getSchoolLocation(@PathVariable("schoolId") String schoolId) {
+        Location location = locationRepository.findOne(Long.parseLong(schoolId.trim()));
         return location;
+    }
+
+    @RequestMapping("/attendance/getAllLocation")
+    @ResponseBody
+    public List<Location> getSchoolLocation() {
+        List<Location> locationList = locationRepository.findAll();
+        return locationList;
+    }
+
+
+
+    @RequestMapping("/attendance/saveLocation/schoolid={schoolid}/longitude={longitude}/latitude={latitude}")
+    @ResponseBody
+    public Location saveSchoolLocation(@PathVariable("schoolid") int schoolId,@PathVariable("longitude") String longitude,@PathVariable("latitude") String latitude) {
+        Location location = new Location();
+        location.setSchool_id(schoolId);
+        location.setLongitude(longitude);
+        location.setLatitude(latitude);
+        return locationRepository.save(location);
+    }
+
+
+    @RequestMapping("/testLocation")
+    @ResponseBody
+    public ArrayList<String> testLocation() {
+        ArrayList<String> result = new ArrayList<String>();
+        result.add("1");
+        result.add("2");
+        result.add("3");
+        result.add("4");
+        return result;
     }
 }
--- src/main/java/com/jgkj/plugin/domain/Location.java
+++ src/main/java/com/jgkj/plugin/domain/Location.java
@@ -1,11 +1,56 @@
 package com.jgkj.plugin.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * Created by chen on 16/10/19.
  */
 @Entity
-public class Location {
+@Table(name="plugin_location")
+public class Location implements Serializable{
+    @Id
+    @GeneratedValue
+    private Long id;
 
+    @Column(nullable = false)
+    private int school_id;
+
+    @Column(nullable = false)
+    private String longitude;
+
+    @Column(nullable = false)
+    private String latitude;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getSchool_id() {
+        return school_id;
+    }
+
+    public void setSchool_id(int school_id) {
+        this.school_id = school_id;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
 }
