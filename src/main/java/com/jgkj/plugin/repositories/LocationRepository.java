09e1ea3325c23bfdd455161d3040328360f26524
chen <38033919@qq.com>
2016-10-22 08:02:02 +0800

    删除无用内容

--- src/main/java/com/jgkj/plugin/repositories/LocationRepository.java
+++ src/main/java/com/jgkj/plugin/repositories/LocationRepository.java
@@ -10,12 +10,7 @@ import org.springframework.data.repository.query.Param;
 import java.io.Serializable;
 
 
-/**
- * @author 程序猿DD
- * @version 1.0.0
- * @date 16/3/23 下午2:34.
- * @blog http://blog.didispace.com
- */
+
 public interface LocationRepository extends JpaRepository<Location, Long>,JpaSpecificationExecutor {
 
 }
--- src/main/java/com/jgkj/plugin/repositories/VideoTimeControllRepository.java
+++ src/main/java/com/jgkj/plugin/repositories/VideoTimeControllRepository.java
@@ -6,12 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
-/**
- * @author 程序猿DD
- * @version 1.0.0
- * @date 16/3/23 下午2:34.
- * @blog http://blog.didispace.com
- */
+
 public interface VideoTimeControllRepository extends JpaRepository<VideoTimeControl, Long>,JpaSpecificationExecutor {
 
 }
