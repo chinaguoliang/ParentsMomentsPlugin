9c65fae8f320395888beeb015f5bb90903bd4025
chen <38033919@qq.com>
2016-11-11 03:11:58 +0800

    增加请求拦截器

---
+++ src/main/java/com/jgkj/plugin/UserSecurityInterceptor.java
@@ -0,0 +1,42 @@
+package com.jgkj.plugin;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserSecurityInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+        Logger.getRootLogger().info("preHandle1");
+
+//        Object obj = request.getSession().getAttribute("cur_user");
+//        if (obj == null || !(obj instanceof UserEntity)) {
+//            response.sendRedirect(request.getContextPath() + "/login");
+//            return false;
+//        }
+
+        return true;//通过验证
+        //return false; //阻止用户请求
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+        Logger.getRootLogger().info("preHandle2");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+
+        Logger.getRootLogger().info("afterCompletion:" + response.getStatus());
+    }
+
+}
